6d62db1fb5f0b6458723231e98a722f9ed987cd9
MartinKamenov <martinkamenov10@gmail.com>
2019-04-24 18:43:54 +0300

    Build websocket demo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import EchoComponent from './components/EchoComponent';
+
+function App() {
+  return (
+    <EchoComponent></EchoComponent>
+  );
+}
+
+export default App;
---
+++ src/components/EchoComponent.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class EchoComponent extends Component {
+    state = { 
+        messages : [] 
+    }
+
+    componentDidMount(){
+        // this is an "echo" websocket service for testing pusposes
+        this.connection = new WebSocket('wss://echo.websocket.org');
+        // listen to onmessage event
+        this.connection.onmessage = evt => { 
+            // add the new message to state
+            this.setState({
+                messages : this.state.messages.concat([ evt.data ])
+            })
+        };
+
+        // for testing: sending a message to the echo service every 2 seconds, 
+        // the service sends it right back
+        setInterval( _ =>{
+            this.connection.send(Math.random());
+        }, 2000 )
+    }
+    render() {
+        // render the messages from state:
+        return <ul>{ this.state.messages.map( (msg, idx) => <li key={'msg-' + idx }>{ msg }</li> )}</ul>;
+    }
+}
+ 
+export default EchoComponent;
