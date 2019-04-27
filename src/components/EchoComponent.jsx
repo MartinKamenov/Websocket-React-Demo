36b55280dc09040934cf6ebc806173e293a6021a
MartinKamenov <martinkamenov10@gmail.com>
2019-04-27 19:59:41 +0300

    Connect to remote api

--- src/components/EchoComponent.jsx
+++ src/components/EchoComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import constants from '../constants/constants';
 
 class EchoComponent extends Component {
     state = { 
@@ -7,23 +8,22 @@ class EchoComponent extends Component {
         number: parseInt(Math.random() * 10, 10)
     }
 
-    componentDidMount(){
+    componentDidMount() {
+        const id = 1;
         // this is an "echo" websocket service for testing pusposes
-        this.connection = new WebSocket('ws://localhost:5000/echo');
+        this.connection = new WebSocket(constants.WS_URL + id);
         // listen to onmessage event
-        this.connection.onmessage = evt => { 
-            debugger;
+        this.connection.onmessage = evt => {
             // add the new message to state
             this.setState({
                 message: evt.data
             })
         };
-        debugger;
 
         // for testing: sending a message to the echo service every 2 seconds, 
         // the service sends it right back
         setInterval(() => {
-            this.connection.send("" + this.state.number);
+            this.connection.send("Message " + this.state.number);
         }, 2000);
     }
     render() {
---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+const constants = {
+    API_URL: 'https://mkchat-api.herokuapp.com/',
+    WS_URL: 'ws://mkchat-api.herokuapp.com/messenger/'
+};
+
+export default constants;
